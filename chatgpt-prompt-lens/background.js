8423cfccdc90a98b10459b0da1642daa14c4ae87
roga <roga@roga.tw>
2025-04-17 14:06:07 +0800

    fix ISSUE3 (128 chars max)

--- chatgpt-prompt-lens/background.js
+++ chatgpt-prompt-lens/background.js
@@ -14,7 +14,7 @@ chrome.action.onClicked.addListener((tab) => {
                         container.setAttribute('id', id);
                         let text = el.innerText.trim();
                         if (text.length > 0) {
-                            text = text.length > 50 ? text.slice(0, 50) + '...' : text;
+                            text = text.length > 128 ? text.slice(0, 128) + '...' : text;
                             text = text.replace(/</g, "&lt;").replace(/>/g, "&gt;");
                             prompts.push({ text, id });
                         }
